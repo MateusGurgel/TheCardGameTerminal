121204d6d060253d75a36693ce5a88b2c56f7e36
Mateus Gurgel <mateusgurgel15@gmail.com>
2024-08-14 01:25:23 -0300

    feat: game logic implemented

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
+from src.modules.agents.generator.battle_generator import BattleGenerator
 from src.modules.agents.generator.scene_generator import SceneGenerator
 
 
 def main() -> None:
 
     scenegenerator = SceneGenerator()
+    battlegenerator = BattleGenerator()
 
-    card_player_one = input("Carta do Jogador 1: ")
-    card_player_two = input("Carta do Jogador 2: ")
+    player_one_score = 0
+    player_two_score = 0
 
-    cenario = scenegenerator.generate(card_player_one, card_player_two)
+    for i in range(0, 3):
+        print(f"Rodada {i + 1}")
 
-    print((cenario))
+        card_player_one = input("Digite o nome do jogador 1: ")
+        card_player_two = input("Digite o nome do jogador 2: ")
+
+        cenario = scenegenerator.generate(card_player_one, card_player_two)
+        battle = battlegenerator.generate(card_player_one, card_player_two, cenario)
+
+        print("Batalha:")
+        print(battle.battle)
+        print("===================================================================")
+
+        if battle.player_one_won:
+            player_one_score += 1
+        else:
+            player_two_score += 1
+
+    if player_one_score > player_two_score:
+        print(f"O jogador 1 venceu com {player_one_score} pontos")
+    elif player_one_score < player_two_score:
+        print(f"O jogador 2 venceu com {player_two_score} pontos")
+    else:
+        print("Empate")
 
 
 if __name__ == "__main__":
